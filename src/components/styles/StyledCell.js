8675bf8199ec70305ea16746fe78af16164a2d9b
mei <yimei.wym@gmail.com>
2019-09-17 22:08:40 +0800

    fix css bug

--- src/components/styles/StyledCell.js
+++ src/components/styles/StyledCell.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 export const StyledCell = styled.div`
   width: auto;
-  height: auto;
   background: rgba(${props => props.color}, 0.7);
   border: ${props => (props.type === 0 ? '0px solid' : '4px solid')};
   border-bottom-color: rgba(${props => props.color}, 0.1);
--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const StyledStage = styled.div`
   display: grid;
-  grid-templates-rows: repeat(
+  grid-template-rows: repeat(
     ${props => props.height}, calc(25vw / ${props => props.width})
     );
   grid-template-columns: repeat(
