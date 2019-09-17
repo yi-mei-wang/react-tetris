7912ec105c57db8f2b898b5c3c02008bfa405a89
mei <yimei.wym@gmail.com>
2019-09-17 22:01:22 +0800

    style game stage

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+// Components
 import Cell from './Cell';
+// Styled-components
+import { StyledStage } from './styles/StyledStage';
 
 const Stage = ({ stage }) => (
-  <div>
+  // stage is an array of arrays
+  // width -> the number of columns, height -> the number of rows
+  <StyledStage width={stage[0].length} height={stage.length}>
     {stage.map(row => row.map((cell, x) => <Cell key={x} type={cell[0]} />))}
-  </div>
+  </StyledStage>
 )
 
 export default Stage;
