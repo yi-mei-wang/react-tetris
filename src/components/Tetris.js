f555d4a813e2be9ec637da985eea8c2582186db6
mei <yimei.wym@gmail.com>
2019-09-29 11:35:36 +0800

    combine new hooks into the main game

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,32 +1,84 @@
-import React from 'react';
-// Helpers
-import { createStage } from '../gameHelpers'
+import React, { useState } from "react";
+
 // Components
-import Stage from './Stage';
-import Display from './Display';
-import StartButton from './StartButton';
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
+
 // Styled components
-import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris'
+import { StyledTetris, StyledTetrisWrapper } from "./styles/StyledTetris";
 
+// Helper function
+import { createStage } from "../gameHelpers";
 
+// Custom hooks
+import { usePlayer } from "../hooks/usePlayer";
+import { useStage } from "../hooks/useStage";
 
 const Tetris = () => {
-  console.log(createStage())
+  const [dropTime, setDropTime] = useState(null);
+  const [gameOver, setGameOver] = useState(false);
 
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [stage, setStage] = useStage(player, resetPlayer);
+
+  console.log("re-render", stage);
+
+  const movePlayer = dir => {
+    updatePlayerPos({ x: dir, y: 0 });
+  };
+
+  const startGame = () => {
+    setStage(createStage());
+    resetPlayer();
+  };
+
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
+
+  const dropPlayer = () => {
+    drop();
+  };
+
+  const move = ({ keyCode }) => {
+    if (!gameOver) {
+      switch (keyCode) {
+        case 37:
+          movePlayer(-1);
+          break;
+        case 39:
+          movePlayer(1);
+          break;
+        case 40:
+          drop();
+          break;
+        default:
+          break;
+      }
+    }
+  };
 
   return (
-    <StyledTetrisWrapper>
+    // 100vw, 100vh to ensure that any keys pressed are registered
+    <StyledTetrisWrapper role="button" tabIndex="0" onKeyDown={e => move(e)}>
       <StyledTetris>
-        <Stage stage={createStage()} />
+        <Stage stage={stage} />
         <aside>
-          <Display text="Score" />
-          <Display text="Rows" />
-          <Display text="Level" />
+          {gameOver ? (
+            <Display gameOver={gameOver} text="Game Over! :(" />
+          ) : (
+            <div>
+              <Display text="Score" />
+              <Display text="Rows" />
+              <Display text="Level" />
+            </div>
+          )}
+          <StartButton callback={startGame} />
         </aside>
-        <StartButton />
       </StyledTetris>
     </StyledTetrisWrapper>
-  )
-}
+  );
+};
 
 export default Tetris;
